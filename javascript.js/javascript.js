491728fa79529e379e35daa3b0d1775c69ec85a3
Juan-parodi23 <ninoparodi2007@gmail.com>
2024-11-26 20:05:38 +0100

    Add files via upload

---
+++ javascript.js/javascript.js
@@ -0,0 +1,14 @@
+alert("7 palabras ESENCIA")
+function cambiarfondo1()
+{document.body.style.backgroundImage = "none";
+   document.body.style.backgroundColor = "black";
+document.body.style.color = "white"
+}
+function cambiarfondo2()
+{alert("me llamo juan y esta es mi pagina llena de esencia")}
+function cambiarfondo3()
+{document.body.style.backgroundImage = "fondo5.jpg";
+ document.body.style.backgroundImage = 'url((fondo.jpg))';
+  document.body.style.backgroundSize = 'cover';
+  document.body.style.backgroundRepeat = 'no-repeat';
+}
